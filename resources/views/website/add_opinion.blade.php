9f22a159426a67b33dae379112f733e36eed038e
Rafał Antochowski <rafal.antochowski@etrust.pl>
2023-01-10 22:47:51 +0100

    Part-10

--- resources/views/website/add_opinion.blade.php
+++ resources/views/website/add_opinion.blade.php
@@ -24,30 +24,41 @@
     <div class="pb-4">
       <p class="text-2xl font-bold">Dodaj opinie</p>
 
-      @if (session('success'))
-      <div class="bg-emerald-100 border-l-4 border-emerald-500 text-emerald-700 p-4 mt-6" role="alert">
-        <p>
-          Twoja opinia została dodana, kiedy zostanie zaakceptowana przez administratora pojawi się na profilu opiekuna.
-        </p>
-      </div>
+      @if (Auth::check() && Auth::user()->role == 'Customer')
 
-      @else 
-        <form method="POST" action="{{ route('website.create_opinion', ['id' => $user->id]) }}">
-          @csrf
+        @if (session('success'))
+        <div class="bg-emerald-100 border-l-4 border-emerald-500 text-emerald-700 p-4 mt-6" role="alert">
+          <p>
+            Twoja opinia została dodana, kiedy zostanie zaakceptowana przez administratora pojawi się na profilu opiekuna.
+          </p>
+        </div>
 
-          <div class="mt-2">
-            <x-select-score />
-          </div>
+        @else 
+          <form method="POST" action="{{ route('website.create_opinion', ['id' => $user->id]) }}">
+            @csrf
 
-          <div class="mt-2">
-            <x-opinion-text />
-          </div>
+            <div class="mt-2">
+              <x-select-score />
+            </div>
 
-          <button type="submit" class="w-full block rounded-md mt-4 bg-emerald-700 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-emerald-600">
-            Dodaj opinie
-          </button>
-        </form>
-      @endif 
+            <div class="mt-2">
+              <x-opinion-text />
+            </div>
+
+            <button type="submit" class="w-full block rounded-md mt-4 bg-emerald-700 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-emerald-600">
+              Dodaj opinie
+            </button>
+          </form>
+        @endif 
+
+      @else
+
+        <div class="bg-gray-100 border-l-4 border-gray-500 text-gray-700 p-4 mt-6" role="alert">
+          <a href="{{ route('login') }}" class="font-bold hover:opacity-75 transition ease-in-out duration-150">Zaloguj się lub stwórz konto klienta</a>
+          <p>Aby dodać opinie dla tego opiekuna musisz być zalogowany jako klient.</p>
+        </div>
+
+      @endif
 
     </div>
 
